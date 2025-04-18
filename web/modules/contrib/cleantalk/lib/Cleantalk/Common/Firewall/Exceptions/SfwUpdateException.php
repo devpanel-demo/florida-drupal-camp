5d445c4522c9e76530ba64d9f649fa07ccca7607
boi <boi@devpanel.com>
2025-04-18 04:48:32 +0000

    Remove tracking contrib module

--- web/modules/contrib/cleantalk/lib/Cleantalk/Common/Firewall/Exceptions/SfwUpdateException.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Cleantalk\Common\Firewall\Exceptions;
-
-class SfwUpdateException extends \Exception
-{
-}
--- web/modules/contrib/cleantalk/lib/Cleantalk/Common/Firewall/Exceptions/SfwUpdateExit.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Cleantalk\Common\Firewall\Exceptions;
-
-class SfwUpdateExit extends \Exception
-{
-}
